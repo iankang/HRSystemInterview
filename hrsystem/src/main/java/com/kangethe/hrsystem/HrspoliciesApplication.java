1ba240c52380e8a97c85e3b52e2f68ec9e2af5ea
iankang <ianngech@gmail.com>
2022-07-26 01:34:01 +0300

    dockerized java application

---
+++ hrsystem/src/main/java/com/kangethe/hrsystem/HrspoliciesApplication.java
@@ -0,0 +1,13 @@
+package com.kangethe.hrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrspoliciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrspoliciesApplication.class, args);
+	}
+
+}
---
+++ hrsystem/src/test/java/com/kangethe/hrsystem/HrspoliciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangethe.hrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrspoliciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
